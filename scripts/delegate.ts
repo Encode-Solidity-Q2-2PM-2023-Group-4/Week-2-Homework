8184c5bf4a87a3c408d68eb60caf8d7f383f4655
linuskelsey <alfred.linus.kelsey@gmail.com>
2023-08-04 17:11:54 +0100

    Add setupProvider()'

--- scripts/delegate.ts
+++ scripts/delegate.ts
@@ -2,6 +2,11 @@ import { ethers } from "ethers";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+function setupProvider() {
+    const provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL ?? "");
+    return provider;
+}
+
 async function main() {
     const to = process.argv.slice(2);
     console.log(`\nDelegating vote to ${to}...`)
